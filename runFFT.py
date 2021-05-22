706b9f1ff9b90736ffa4dc263f8b9bacc896ecdf
Dominic <draico195@gmail.com>
2021-05-22 10:36:30 -0400

    added folder searching

--- runFFT.py
+++ runFFT.py
@@ -1,18 +1,26 @@
+import os
 import scipy.io.wavfile as wavfile
 import scipy
 import scipy.fftpack as fftpk
 import numpy as np
 from matplotlib import pyplot as plt
 
-fileName = "exampleOhm.wav"
+folder = "./soundFiles"
 
-s_rate, signal = wavfile.read(fileName)       # read file
-FFT = abs(scipy.fft(signal))                   # do FFT
-freqs = fftpk.fftfreq(len(FFT),(1.0/s_rate))   # make frequency vector
+files = os.listdir(folder)
 
-#plot
-plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])
-plt.xlabel("Frequency (Hz)")
-plt.ylabel("Amplitude")
-plt.Text(0, 0.5, 'Amplitude')
-plt.show()
+for file in files:
+
+  currentDirectory = os.getcwd()
+  print(currentDirectory + "/" + folder + "/"  + file)
+  s_rate, signal = wavfile.read(currentDirectory + "/" + folder + "/"  + file)    # read file
+  FFT = abs(scipy.fft(signal))                              # do FFT
+  freqs = fftpk.fftfreq(len(FFT),(1.0/s_rate))              # make frequency vector
+
+  #plot
+  plt.plot(freqs[range(len(FFT)//2)], FFT[range(len(FFT)//2)])
+  plt.xlabel("Frequency (Hz)")
+  plt.ylabel("Amplitude")
+  plt.Text(0, 0.5, 'Amplitude')
+  plt.show()
+
